cd27722d58e29803b8e4303442ef0cf4e933eb6e
Egan McComb <egan.mccomb@gmail.com>
2014-08-16 15:34:30 -0700

    Initial commit of jscommander2, which uses serial library for direct connection to serial device.

---
+++ test/jsservo/src/jscommander2.c
@@ -0,0 +1,137 @@
+/*
+ * Joystick interface and commander.
+ *
+ * This code was adapted from GPL licensed:
+ *
+ * > jstest.c  Version 1.2
+ *
+ * > Copyright (c) 1996-1999 Vojtech Pavlik
+ *
+ * > Sponsored by SuSE
+ */
+
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#include <linux/joystick.h>
+
+#include "serial.h"
+
+#define NAME_LENGTH 128
+
+int main(int argc, char **argv)
+{
+	/* Serial device. */
+	const int buf_max = 256;
+	int timeout = 5000;
+	int sp = -1;
+	int rc;
+	int baudrate = 19200; // Baud rate used on Arduino event handler.
+	char serialport[buf_max];
+	char buf[buf_max];
+
+	/* Joystick device. */
+	int fd;
+	unsigned char axes = 2;
+	unsigned char buttons = 2;
+	int version = 0x000800;
+	char name[NAME_LENGTH] = "Unknown";
+
+
+	/* Argument handling. */
+	if (argc != 2 || !strcmp("--help", argv[1])) {
+		puts("Usage: jscommander <device>");
+		exit(1);
+	}
+	if ((fd = open(argv[argc - 1], O_RDONLY)) < 0) {
+		perror("jscommander: Unable to open joystick device");
+		exit(1);
+	}
+
+	/* Serial device setup. */
+	/* XXX: Add argument to select serial device */
+	strcpy(serialport, "/dev/ttyACM0");
+	sp = serialport_init(serialport, baudrate);
+	if (fd==-1) perror("jscommander: Could not open serial device");
+	serialport_flush(sp);
+
+	/* Joystick setup. */
+	ioctl(fd, JSIOCGVERSION, &version);
+	ioctl(fd, JSIOCGAXES, &axes);
+	ioctl(fd, JSIOCGBUTTONS, &buttons);
+	ioctl(fd, JSIOCGNAME(NAME_LENGTH), name);
+
+	printf("Joystick (%s) has %d axes and %d buttons. Driver version is %d.%d.%d.\n",
+		name, axes, buttons, version >> 16, (version >> 8) & 0xff, version & 0xff);
+	printf("Running... (SIGINT to exit)\n");
+
+	/*
+	 * Event interface.
+	 *
+	 * For PS2 controller via usb adapter:
+	 * 		Left JS:     Axes 0 and 1
+	 * 		Left click:  Button 10
+	 * 		Right JS:    Axes 2 and 3
+	 * 		Right click: Button 11
+	 * 		D Pad:       Axes 4 and 5
+	 * 		Select:      Button 8
+	 * 		Start:       Button 9
+	 * 		Left 1:      Button 6
+	 * 		Left 2:      Button 4
+	 * 		Right 1:     Button 7
+	 * 		Right 2:     Button 5
+	 * 		Triangle:    Button 0
+	 * 		Circle:      Button 1
+	 * 		Cross:       Button 2
+	 * 		Box:         Button 3
+	 */
+	int *axis;
+	int *button;
+	int i;
+	struct js_event js;
+
+	axis = calloc(axes, sizeof(int));
+	button = calloc(buttons, sizeof(char));
+
+	while (1) {
+		if (read(fd, &js, sizeof(struct js_event)) != sizeof(struct js_event)) {
+			perror("jscommander: Error reading joystick device");
+			exit (1);
+		}
+
+		switch(js.type & ~JS_EVENT_INIT) {
+			case JS_EVENT_BUTTON:
+				button[js.number] = js.value;
+				break;
+			case JS_EVENT_AXIS:
+				axis[js.number] = js.value;
+				break;
+		}
+
+		printf("\r");
+
+		/* Use axes 0 and 1 to control servos. */
+		if (axes) {
+			sprintf(buf, "%dx%dy", axis[0]/364+90, axis[1]/364+90);
+			rc = serialport_write(sp, buf);
+			if (rc == -1) perror("jscommander: Error writing to serial device");
+		}
+
+		//if (buttons) {
+		//	printf("Buttons: ");
+		//	for (i = 0; i < buttons; i++)
+		//		printf("%2d:%s ", i, button[i] ? "on " : "off");
+		//}
+
+		fflush(stdout);
+	}
+}
+
+// vim: tabstop=4
---
+++ test/jsservo/src/serial.h
@@ -0,0 +1,1 @@
+/home/egan/code/share/rov/lib/serial.h
